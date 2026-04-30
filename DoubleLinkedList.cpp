91cce92c1667c0cf7c956be8d285c76fd2d957ac
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-30 17:56:31 +0700

    membuat node baru

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -27,3 +27,9 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
